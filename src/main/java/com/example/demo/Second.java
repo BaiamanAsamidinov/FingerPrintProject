7f750bfd57956da03e7a8acba650cede0288f843
BaiamanAsamidinov <bayaman.asamidinov1@gmail.com>
2020-05-20 12:52:30 +0600

    Second

--- src/main/java/com/example/demo/Second.java
+++ src/main/java/com/example/demo/Second.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FirstPage {
-
-        @RequestMapping("/")
-        public String helloWorld(){
-            return "First!!";
-        }
-
+public class Second {
+    @RequestMapping("/")
+    public String helloWorld(){
+        return "second!!";
+    }
 }
+
