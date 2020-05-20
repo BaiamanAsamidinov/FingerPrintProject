98d6af8c56ee748168548713866619fc6457aa2b
BaiamanAsamidinov <bayaman.asamidinov1@gmail.com>
2020-05-20 12:33:00 +0600

    Project added to git

---
+++ src/main/java/com/example/demo/FirstPage.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstPage {
+
+        @RequestMapping("/")
+        public String helloWorld(){
+            return "Salam duinoooo!!";
+        }
+
+}
